748a9111b056926747b6a3eadd967d592fd92fd0
lucasd <lucasd@udel.edu>
2022-11-19 17:04:56 -0500

    tested string whitelist and blacklist

--- test/PyJoi_test/String/test_StringSchema.py
+++ test/PyJoi_test/String/test_StringSchema.py
@@ -1,12 +1,9 @@
 import unittest
 import PyJoi
-import typing
 from PyJoi.Primitive.String import Exceptions
-
-
+from PyJoi.Primitive import Exceptions as PrimitiveExceptions
 
 class TestStringSchema(unittest.TestCase):
-
     def test_optional_accepts_missing(self):
         value = PyJoi.Schema().string().optional().validate(None)
         self.assertIsNone(value)
@@ -22,6 +19,44 @@ class TestStringSchema(unittest.TestCase):
     def test_too_long_fails(self):
         with self.assertRaises(Exceptions.TooLongException):
             PyJoi.Schema().string().max_len(3).validate("hello")
+
+    def test_inside_range_accepted(self):
+        value = PyJoi.Schema().string().min_len(3).max_len(10).validate("hello")
+        self.assertEqual(value,"hello")
+
+    def test_accepts_in_whitelist(self):
+        value = PyJoi.Schema().string().whitelist("hi").validate("hi")
+        self.assertEqual(value,"hi")
+        s = PyJoi.Schema().string().whitelist(["hi","hello"])
+        self.assertEqual(s.validate("hi"),"hi")
+        self.assertEqual(s.validate("hello"),"hello")
+
+    def test_whitelist_rejects_outsiders(self):
+        with self.assertRaises(PrimitiveExceptions.NonWhiteListedValueException):
+            PyJoi.Schema().string().whitelist("hi").validate("hello")
+        s = PyJoi.Schema().string().whitelist(["hi", "hello"])
+        with self.assertRaises(PrimitiveExceptions.NonWhiteListedValueException):
+            s.validate("yes")
+
+    def test_blacklist_accepts_outsiders(self):
+        value = PyJoi.Schema().string().blacklist("hi").validate("hello")
+        self.assertEqual(value,"hello")
+        value = PyJoi.Schema().string().blacklist(["hi", "hello"]).validate("yes")
+        self.assertEqual(value,"yes")
+
+    def test_blacklist_rejects_outsiders(self):
+        with self.assertRaises(PrimitiveExceptions.BlackListedValueException):
+            PyJoi.Schema().string().blacklist("hi").validate("hi")
+        s = PyJoi.Schema().string().blacklist(["hi","hello"])
+        with self.assertRaises(PrimitiveExceptions.BlackListedValueException):
+            s.validate("hi")
+        with self.assertRaises(PrimitiveExceptions.BlackListedValueException):
+            s.validate("hello")
+
+
+    
+
+    
 
     
 
