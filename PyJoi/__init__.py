355914920279f985c0449371bb58d164b4b40ced
lucasd <lucasd@udel.edu>
2023-01-30 19:19:46 -0500

    Fixed __init__.py type hints

--- PyJoi/__init__.py
+++ PyJoi/__init__.py
@@ -2,28 +2,28 @@ from .SchemaSrc import Schema
 from .RefSrc import Ref
 from .Primitive.Str import StrSchema
 from .Primitive.Numeric.Int import IntSchema
-from .Iterable.List.ListSchema import ListSchema
-from .Iterable.IterableSchema import IterableSchema
-from .Iterable.Set.SetSchema import SetSchema
+from .Iterable.List import ListSchema
+from .Iterable import IterableSchema
+from .Iterable.Set import SetSchema
 from .Primitive.Bool import BoolSchema
 import typing
-
-def str(name: typing.Optional[str] = None)->StrSchema.StrSchema[str]:
-    """Create a string schema."""
-    return StrSchema.StrSchema(name)
 
 def int(name: typing.Optional[str] = None)->IntSchema.IntSchema[int]:
     """Create an int schema."""
     return IntSchema.IntSchema(name)
 
-def list(name: typing.Optional[str] = None)->ListSchema:
-    return ListSchema(name)
+def list(name: typing.Optional[str] = None)->ListSchema.ListSchema[typing.List]:
+    return ListSchema.ListSchema(name)
 
-def iterable(name: typing.Optional[str] = None)->IterableSchema:
-    return IterableSchema(name)
+def iterable(name: typing.Optional[str] = None)->IterableSchema.IterableSchema[typing.Iterable]:
+    return IterableSchema.IterableSchema(name)
 
-def set(name: typing.Optional[str] = None)->SetSchema:
-    return SetSchema(name)
+def set(name: typing.Optional[str] = None)->SetSchema.SetSchema[typing.Set]:
+    return SetSchema.SetSchema(name)
 
-def bool(name: typing.Optional[str] = None)->BoolSchema:
+def bool(name: typing.Optional[str] = None)->BoolSchema[bool]:
     return BoolSchema.BoolSchema(name)
+
+def str(name: typing.Optional[str] = None)->StrSchema.StrSchema[str]:
+    """Create a string schema."""
+    return StrSchema.StrSchema(name)
